902e9ea5ac9d4ad865613d73cdeb7a904f4d3bb7
Aman Purohit <purohiam@sheridancollege.ca>
2024-07-09 02:22:28 -0400

    Update ViewCreator.css and ViewCreator.js

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./index.css";
 const Form = ({ onSubmit, editMode, onDelete, creator }) => {
   const creatorData = creator || {};
   const handleSubmit = (event) => {
@@ -15,98 +15,108 @@ const Form = ({ onSubmit, editMode, onDelete, creator }) => {
   };
 
   return (
-    <form class="container" onSubmit={handleSubmit}>
-      <fieldset>
-        <label>
-          Name
-          <input name="name" defaultValue={creatorData?.name} />
-        </label>
-        <label aria-label="Image" aria-describedby="image-helper">
-          Image {<br />}
-          <small id="image-helper">
-            <em>
-              Provide a link to an image of your creator. Be sure to include the
-              http://
-            </em>
-          </small>
-        </label>
+    <form class="form-container" onSubmit={handleSubmit}>
+      <div class="container">
+        <fieldset>
+          <label>
+            Name
+            <input name="name" defaultValue={creatorData?.name} />
+          </label>
+          <label aria-label="Image" aria-describedby="image-helper">
+            Image {<br />}
+            <small id="image-helper">
+              <em>
+                Provide a link to an image of your creator. Be sure to include
+                the http://
+              </em>
+            </small>
+          </label>
 
-        <input
-          type="url"
-          name="imageUrl"
-          defaultValue={creatorData?.imageURL}
-        />
+          <input
+            type="url"
+            name="imageUrl"
+            defaultValue={creatorData?.imageURL}
+          />
 
-        <label>
-          Description{<br />}
-          <small>
-            <em>
-              Provide a description of the creator. Who are they? What makes
-              them interesting?
-            </em>
-          </small>
-        </label>
+          <label>
+            Description{<br />}
+            <small>
+              <em>
+                Provide a description of the creator. Who are they? What makes
+                them interesting?
+              </em>
+            </small>
+          </label>
 
-        <input
-          type="text"
-          name="description"
-          defaultValue={
-            creatorData?.description ? creatorData?.description : "   "
-          }
-        />
-      </fieldset>
-      <legend>Social Media Links</legend>
-      <small>
-        <em>Provide at least one of the creator's social media links</em>
-      </small>
-      <div class="social">
-        <label>
-          Youtube{<br />}
-          <small>
-            <em>The creator's YouTube handle (without the @)</em>
-          </small>
-        </label>
+          <input
+            type="text"
+            name="description"
+            defaultValue={
+              creatorData?.description ? creatorData?.description : "   "
+            }
+          />
+        </fieldset>
+        <legend>Social Media Links</legend>
+        <small>
+          <em>Provide at least one of the creator's social media links</em>
+        </small>
+        <div class="social">
+          <label>
+            Youtube{<br />}
+            <small>
+              <em>The creator's YouTube handle (without the @)</em>
+            </small>
+          </label>
 
-        <input
-          type="text"
-          name="youtube"
-          defaultValue={creatorData?.url?.[0]}
-        />
-      </div>
-      <div class="social">
-        <label>
-          Twitter{<br />}{" "}
-          <small>
-            <em>The creator's Twitter handle (without the @)</em>
-          </small>
-        </label>
+          <input
+            type="text"
+            name="youtube"
+            defaultValue={creatorData?.url?.[0]}
+          />
+        </div>
+        <div class="social">
+          <label>
+            Twitter{<br />}{" "}
+            <small>
+              <em>The creator's Twitter handle (without the @)</em>
+            </small>
+          </label>
 
-        <input
-          type="text"
-          name="twitter"
-          defaultValue={creatorData?.url?.[1]}
-        />
-      </div>
-      <div class="social">
-        <label>
-          Instagram{<br />}
-          <small>
-            <em>The creator's Instagram handle (without the @)</em>
-          </small>
-        </label>
+          <input
+            type="text"
+            name="twitter"
+            defaultValue={creatorData?.url?.[1]}
+          />
+        </div>
+        <div class="social">
+          <label>
+            Instagram{<br />}
+            <small>
+              <em>The creator's Instagram handle (without the @)</em>
+            </small>
+          </label>
 
-        <input
-          type="text"
-          name="instagram"
-          defaultValue={creatorData?.url?.[2]}
-        />
-        <button type="submit">SUBMIT</button>
+          <input
+            type="text"
+            name="instagram"
+            defaultValue={creatorData?.url?.[2]}
+          />
+          <section className={`grid ${editMode ? "btn-div" : ""}`}>
+            <button type="submit" className="button">
+              SUBMIT
+            </button>
 
-        {editMode && (
-          <button onClick={handleDelete} type="button">
-            DELETE
-          </button>
-        )}
+            {editMode && (
+              <button
+                className="delete button"
+                onClick={handleDelete}
+                type="button"
+              >
+                DELETE
+              </button>
+            )}
+          </section>
+        </div>
       </div>
     </form>
   );
--- src/pages/ViewCreator.js
+++ src/pages/ViewCreator.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import { redirect, useParams, useNavigate } from "react-router-dom";
 import { supabase, getCreatorById, deleteCreator } from "../client.js";
 import "./ViewCreator.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle, faPen } from "@fortawesome/free-solid-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 const ViewCreator = () => {
   const navigate = useNavigate();
   const { creatorId } = useParams();
@@ -23,10 +28,32 @@ const ViewCreator = () => {
       setCreator(data);
     });
   }, []);
+  const [clickedyt, setClickedyt] = useState(false);
+  const [clickedig, setClickedig] = useState(false);
+  const [clickedtw, setClickedtw] = useState(false);
+
+  const handleClickYt = () => {
+    setClickedyt(!clickedyt);
+    setClickedig(false);
+    setClickedtw(false);
+  };
+  const handleClickIg = () => {
+    setClickedig(!clickedig);
+    setClickedyt(false);
+    setClickedtw(false);
+  };
+  const handleClickTw = () => {
+    setClickedtw(!clickedtw);
+    setClickedyt(false);
+    setClickedig(false);
+  };
+  const handleClick = (event) => {
+    event.preventDefault();
+  };
 
   return (
-    <div class="container">
-      <div class="grid">
+    <div class="container view">
+      <div class="creator-info">
         <section class="img-container">
           <img class="img" src={creator?.imageURL} alt="placeholder" />
         </section>
@@ -34,18 +61,59 @@ const ViewCreator = () => {
           <div class="text-container">
             <h1>{creator?.name}</h1>
             <p>{creator?.description}</p>
-            <p> Youtube = {creator?.url[0]}</p>
-            <p>Instagram = {creator?.url[1]}</p>
-            <p>Twitter = {creator?.url[2]}</p>
+
+            <div class="social-icons">
+              <div>
+                {creator?.url[0] ? (
+                  <a
+                    href={`https://www.youtube.com/channel/${creator?.url[0]}`}
+                    target="_blank"
+                    onClick={handleClickYt}
+                    className={clickedyt ? "clicked" : ""}
+                  >
+                    <FontAwesomeIcon size="2x" icon={faYoutube} />
+                    <span> {creator?.url[0]}</span>
+                  </a>
+                ) : null}
+
+                {creator?.url[1] && (
+                  <a
+                    href={`https://www.instagram.com/${creator?.url[1]}`}
+                    target="_blank"
+                    onClick={handleClickIg}
+                    className={clickedig ? "clicked" : ""}
+                  >
+                    <FontAwesomeIcon size="2x" icon={faInstagram} />
+                    <span> {creator?.url[1]}</span>
+                  </a>
+                )}
+                {creator?.url[2] && (
+                  <a
+                    href={`https://twitter.com/${creator?.url[2]}`}
+                    target="_blank"
+                    onClick={handleClickTw}
+                    className={clickedtw ? "clicked" : ""}
+                  >
+                    <FontAwesomeIcon size="2x" icon={faTwitter} />
+                    <span> {creator?.url[2]}</span>
+                  </a>
+                )}
+              </div>
+            </div>
             {/* <p>{creator?.url}</p> */}
           </div>
         </section>
       </div>
       <section class="btn-container grid">
-        <a href={`/edit/${creatorId}`} type="button">
+        <a class="button" href={`/edit/${creatorId}`} type="button">
           EDIT
         </a>
-        <a href={`/`} onClick={(event) => handleDelete(event)} type="button">
+        <a
+          class="button delete"
+          href={`/`}
+          onClick={(event) => handleDelete(event)}
+          type="button"
+        >
           DELETE
         </a>
       </section>
