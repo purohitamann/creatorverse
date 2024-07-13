bfc43e327bad96964b7fd2a4b6489541ef034954
Aman Purohit <purohiam@sheridancollege.ca>
2024-07-13 09:41:47 -0400

    updated database configurations

--- src/client.js
+++ src/client.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const URL = "https://tinwcbhvaklecussydua.supabase.co";
-const API =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpbndjYmh2YWtsZWN1c3N5ZHVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAyODEwMjksImV4cCI6MjAzNTg1NzAyOX0.F2ggA-qGrrHrIG56Hzgcd4Hsyh3gggaxKibPnzH1GX4";
+const URL = process.env.REACT_APP_SUPABASE_URL;
+const API = process.env.REACT_APP_SUPABASE_KEY;
 export const supabase = createClient(URL, API);
 
 export async function addItem(item) {
